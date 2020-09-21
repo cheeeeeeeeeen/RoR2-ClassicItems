85270647988f406c44a56e60a994bb3c21840973
cheeeeeeeeeen <blancfaye7@gmail.com>
2020-09-21 22:49:00 +0800

    Add BetterUI as soft dependency for Catalog support

---
+++ Compat/BetterUI.cs
@@ -0,0 +1,16 @@
+﻿namespace Chen.ClassicItems
+{
+    public static class Compat_BetterUI
+    {
+        private static bool? _enabled;
+
+        internal static bool enabled
+        {
+            get
+            {
+                if (_enabled == null) _enabled = BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.xoxfaby.BetterUI");
+                return (bool)_enabled;
+            }
+        }
+    }
+}
