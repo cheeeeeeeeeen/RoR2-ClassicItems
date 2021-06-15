3442f284118fd95b460aaac62f4fa6348a84624f
cheeeeeeeeeen <blancfaye7@gmail.com>
2021-06-15 20:42:29 +0800

    Integrate generic Content provider from ChensHelpers

--- ChensClassicItems/ContentProvider.cs
+++ ChensClassicItems/ContentProvider.cs
@@ -1,46 +1,16 @@
 ﻿using Chen.ClassicItems.Artifacts;
 using RoR2.ContentManagement;
-using System.Collections;
 
 namespace Chen.ClassicItems
 {
-    internal class ContentProvider : IContentPackProvider
+    internal class ContentProvider : GenericContentPackProvider
     {
-        internal ContentPack contentPack = new ContentPack();
+        protected override string ContentIdentifier() => ClassicItemsPlugin.ModGuid;
 
-        public string identifier => ClassicItemsPlugin.ModGuid;
-
-        public void Initialize()
+        protected override void LoadStaticContentAsyncActions(LoadStaticContentAsyncArgs args)
         {
-            ContentManager.collectContentPackProviders += ContentManager_collectContentPackProviders;
-        }
-
-        private void ContentManager_collectContentPackProviders(ContentManager.AddContentPackProviderDelegate addContentPackProvider)
-        {
-            addContentPackProvider(this);
-        }
-
-        public IEnumerator LoadStaticContentAsync(LoadStaticContentAsyncArgs args)
-        {
-            contentPack.identifier = identifier;
             contentPack.bodyPrefabs.Add(Origin.bodyObjects.ToArray());
             contentPack.masterPrefabs.Add(Origin.masterObjects.ToArray());
-
-            args.ReportProgress(1f);
-            yield break;
-        }
-
-        public IEnumerator GenerateContentPackAsync(GetContentPackAsyncArgs args)
-        {
-            ContentPack.Copy(contentPack, args.output);
-            args.ReportProgress(1f);
-            yield break;
-        }
-
-        public IEnumerator FinalizeAsync(FinalizeAsyncArgs args)
-        {
-            args.ReportProgress(1f);
-            yield break;
         }
     }
 }
